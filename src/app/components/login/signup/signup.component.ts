76703122487f07155888ae27b9ca0cf1a177e57f
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-09 16:26:52 +0530

    signup form

--- src/app/components/login/signup/signup.component.ts
+++ src/app/components/login/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { customer } from 'src/app/Models/customer';
 import { CustomerService } from 'src/app/services/customer.service';
@@ -10,32 +10,45 @@ import { CustomerService } from 'src/app/services/customer.service';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
-  formValue!: FormGroup;
+  formValue: FormGroup;
+  passwordsMismatch: boolean = false;
   CustomerModelObj: customer = new customer();
 
   constructor(
     private formBuilder: FormBuilder,
     private api: CustomerService,
     private route: Router
-  ) {}
-
-  ngOnInit(): void {
+  ) {
     this.formValue = this.formBuilder.group({
       customerName: [''],
       email: [''],
       password: [''],
+      confirmPassword: [''],
       phoneNo: [''],
     });
   }
 
   onCustomerSignup() {
-    this.CustomerModelObj.customerName = this.formValue.value.customerName;
-    this.CustomerModelObj.email = this.formValue.value.email;
-    this.CustomerModelObj.password = this.formValue.value.password;
-    this.CustomerModelObj.phoneNumber = this.formValue.value.phoneNo;
+    const password = this.formValue.get('password')?.value;
+    const confirmPassword = this.formValue.get('confirmPassword')?.value;
 
-    this.api.customerSignup(this.CustomerModelObj).subscribe((res) => {
-      console.log(res);
-    });
+    if (password !== confirmPassword) {
+      this.passwordsMismatch = true;
+    } else {
+      this.passwordsMismatch = false;
+      this.CustomerModelObj.customerName = this.formValue.value.customerName;
+      this.CustomerModelObj.email = this.formValue.value.email;
+      this.CustomerModelObj.password = this.formValue.value.password;
+      this.CustomerModelObj.phoneNumber = this.formValue.value.phoneNo;
+
+      this.api.customerSignup(this.CustomerModelObj).subscribe((res) => {
+        if (res.isSuccess) {
+          console.log(res);
+          this.route.navigate(['/home']);
+        } else {
+          alert(res.message);
+        }
+      });
+    }
   }
 }
